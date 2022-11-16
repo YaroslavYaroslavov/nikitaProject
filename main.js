65fe7bbd59860995ae37ee963037e0f9175a5503
Yaroslav <tvoibaget@mail.ru>
2022-11-17 00:10:33 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const backdrop = document.querySelector('.backdrop')
+const loginPopUp = document.querySelector('.popupLogin')
+const headerLoginBtn = document.querySelector('.headerLogin')
+const cansel = document.querySelector('.cansel')
+const popUpLoginBtn = document.querySelector('.popUpLoginBtn')
+const thisIsMyOrder = document.querySelectorAll('.thisIsMyOrder')
+const popupCard = document.querySelectorAll('.popupCard')
+const closePopUp = document.querySelectorAll('.closePopUp')
+
+console.log(closePopUp)
+
+closePopUp.forEach((elem, i) => {
+    elem.addEventListener('click', () => {
+        backdrop.style.display = 'none'
+        popupCard[i].style.display = 'none'
+    })
+});
+thisIsMyOrder.forEach((elem, i) => {
+    elem.addEventListener('click', () => {
+        backdrop.style.display = 'block'
+        popupCard[i].style.display = 'flex'
+    })
+});
+
+headerLoginBtn.addEventListener('click', () => {
+    backdrop.style.display = 'block'
+    loginPopUp.style.display = 'flex'
+})
+cansel.addEventListener('click', () => {
+    backdrop.style.display = 'none'
+    loginPopUp.style.display = 'none'
+})
+popUpLoginBtn.addEventListener('click', () => {
+        window.location.href = './profile.html'
+    })
+    // console.log(headerLoginBtn)
+    // headerLoginBtn.addEventListener('click', () => { console.log(1) })
